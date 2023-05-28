1cd61048eb7523b1b697af38cdea2a5c147659e5
SivaSurender <e.surender14@gmail.com>
2023-05-28 12:34:03 +0530

    initial setup

---
+++ Initial.js
@@ -0,0 +1,13 @@
+console.log("Hello from node");
+
+// Import which allows to write files to system
+const fileSystem = require("fs");
+
+// write file sync from import
+
+/**
+ * takes in two args
+ *  first one is file name anbd type, path
+ * second one is content
+ */
+fileSystem.writeFileSync("test.txt", "Hello from fs of node");
